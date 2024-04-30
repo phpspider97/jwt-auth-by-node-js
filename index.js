f72a6f45ab9df87992731ad257859fd95fa7f971
Neelmani <90858177+phpspider97@users.noreply.github.com>
2024-04-30 18:43:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 import 'dotenv/config'
 import mongoose from 'mongoose'
-mongoose.connect('mongodb+srv://neelbhardwaj97:admin123@cluster0.nr9e50u.mongodb.net/test')
+mongoose.connect('mongodb+srv://name:password@cluster0.nr9e50u.mongodb.net/test')
 const PORT = process.env.PORT || 4200
 
 app.get('/',()=>{
